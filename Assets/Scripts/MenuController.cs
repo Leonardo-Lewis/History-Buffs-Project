87ac012ea2122f972fc18ef4c6209a081355aeb4
Leonardo-Lewis <leonardo.lee.lewis@gmail.com>
2022-06-07 09:05:30 +1000

    Quit button

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -9,4 +9,9 @@ public class MenuController : MonoBehaviour
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
+    public void QuitGame()
+    {
+        Debug.Log("QUIT!");
+        Application.Quit();
+    }
 }
